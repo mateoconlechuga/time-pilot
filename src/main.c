77f7effe8779e315515a26cd677b09f3d4cc6e43
Matt Waltz <matthewwaltzis@gmail.com>
2017-07-21 21:31:06 -0600

    Added images

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include <tice.h>
+
+#include <graphx.h>
+#include <fileioc.h>
+#include <keypadc.h>
+
+#include "gfx/level1_gfx.h"
+#include "gfx/level2_gfx.h"
+#include "gfx/level3_gfx.h"
+#include "gfx/level4_gfx.h"
+#include "gfx/level5_gfx.h"
+#include "gfx/other_gfx.h"
+
+void main(void) {
+    gfx_Begin();
+    gfx_End();
+}
